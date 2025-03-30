1f7e208d1046b6f16735981f0972d4dd998b325d
Jorge <jorgejch@gmail.com>
2025-03-30 14:09:34 -0300

    Set default value for kubeconfig_path variable
    Signed-off-by: Jorge <jorgejch@gmail.com>

--- example/variables.tf
+++ example/variables.tf
@@ -1,8 +1,5 @@
 variable "kubeconfig_path" {
   description = "Path to your Kubernetes kubeconfig file"
   type        = string
-}
-variable "example_variable" {
-  description = "Example input variable for Arbitrum module"
-  type        = string
+  default = "~/.kube/config"
 }
